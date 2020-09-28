56104e34e6c87d3bce394642ce264fc559e97532
MaximTomchin <tomka1245@gmail.com>
2020-09-28 14:45:44 +0300

    исправления

--- srcipts/index.js
+++ srcipts/index.js
@@ -1,26 +1,25 @@
-console.log ('Hallo, Welt');
-
 let buttonOpenPopup = document.querySelector('.profile__edit-button');
 let buttonClosePopup = document.querySelector('.popup__close-image');
 let popup = document.querySelector('.popup');
-
-let popupTuggle = () => {
-popup.classList.toggle('popup_opened');
-}
-
-buttonOpenPopup.addEventListener('click',popupTuggle);
-buttonClosePopup.addEventListener('click',popupTuggle);
-
- 
 let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__field[name="name"]');
+let jobInput = document.querySelector('.popup__field[name="about"]'); 
+let profile = document.querySelector('.profile__name');
+let description = document.querySelector('.profile__description');
 
-let formSubmitHandler  = (evt) => {
-    evt.preventDefault();
-    let nameInput = document.querySelector('.popup__field[name="name"]');
-    let jobInput = document.querySelector('.popup__field[name="about"]');  
-    document.querySelector('.profile__name').textContent = nameInput.value;     
-    document.querySelector('.profile__description').textContent = jobInput.value; 
+let togglePopup = () => {
+    nameInput.value = profile.textContent;
+    jobInput.value = description.textContent;
     popup.classList.toggle('popup_opened');
 }
 
+let formSubmitHandler  = (evt) => {
+    evt.preventDefault();
+    profile.textContent = nameInput.value;     
+    description.textContent = jobInput.value; 
+    popup.classList.toggle('popup_opened');
+}
+
+buttonOpenPopup.addEventListener('click',togglePopup);
+buttonClosePopup.addEventListener('click',togglePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
