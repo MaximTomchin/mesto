b7c20a741f18e6f7def51da87ebeb80f144b4410
MaximTomchin <tomka12452@gmail.com>
2020-11-18 11:37:19 +0300

    исправления проектой работы 7

--- srcipts/utils.js
+++ srcipts/utils.js
@@ -1,5 +1,3 @@
-const overlay = document.querySelector('.popup__overlay');
-
 const closePopupByESC = (evt) => {
     if (evt.key === 'Escape') {
         togglePopup(document.querySelector('.popup_opened'));
@@ -11,10 +9,12 @@ const togglePopup = (popup) => {
     
     if (popup.classList.contains('popup_opened')) {
         document.addEventListener('keydown', closePopupByESC);
-        overlay.addEventListener('click', togglePopup(popup));
+        popup.addEventListener('click', () => togglePopup(popup));
+     
     } else {
         document.removeEventListener('keydown', closePopupByESC);
-        overlay.removeEventListener('click', togglePopup(popup)); 
+        popup.removeEventListener('click', () => togglePopup(popup)); 
+    
     };
 };
 
